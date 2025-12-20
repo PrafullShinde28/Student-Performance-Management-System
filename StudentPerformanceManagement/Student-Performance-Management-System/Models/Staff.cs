c624bf933980c9bea4e67d893baf6c7b54856998
vishwadeep sankpal <vishwadeep108@gmail.com>
2025-12-20 15:43:11 +0530

    Added Staff and Student model

---
+++ StudentPerformanceManagement/Student-Performance-Management-System/Models/Staff.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Student_Performance_Management_System.Models
+{
+    public class Staff
+    {
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string MobileNo { get; set; }
+        public ICollection<Tasks> Tasks = new List<Tasks>();
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
---
+++ StudentPerformanceManagement/Student-Performance-Management-System/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace Student_Performance_Management_System.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string PRN { get; set; }
+        public string Name { get; set; }
+        public string? Email { get; set; }
+        public string? MobileNo { get; set; }
+        public int CourseID { get; set; }
+        public Course Course { get; set; }
+        public int CourseGroupId { get; set; }
+        public CourseGroup courseGroup { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
