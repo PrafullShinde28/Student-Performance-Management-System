ad9137e16def8ae4c2f6ca386236ad21fcbd807e
Prafull Shinde <prafullshinde7007@gmail.com>
2025-12-22 13:53:04 +0530

    subject model added

--- StudentPerformanceManagement/Student-Performance-Management-System/Models/Subjects.cs
+++ StudentPerformanceManagement/Student-Performance-Management-System/Models/Subjects.cs
@@ -1,6 +1,28 @@
-﻿namespace Student_Performance_Management_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student_Performance_Management_System.Models
 {
     public class Subjectcs
     {
+        public int SubjectId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string SubjectName { get; set; }
+
+        public int CourseId { get; set; }
+        public Course Course { get; set; } = null;
+
+        public int MaxTheoryMarks { get; set; } = 40;
+
+        public int MaxLabMarks { get; set; } = 40;
+
+        public int MaxInternalMarks { get; set; } = 20;
+
+        public int PassingPercentTotal { get; set; } = 40;
+
+        public int PassingPercentEachComponent { get; set; } = 40;
+
+        public Marks Marks { get; set; }
     }
 }
