0f8223b43a32b2845ea9f7e9bd28d279f1747076
Prafull Shinde <prafullshinde7007@gmail.com>
2025-12-22 13:46:13 +0530

    updated gitignore file added

--- StudentPerformanceManagement/Student-Performance-Management-System/Models/Marks.cs
+++ StudentPerformanceManagement/Student-Performance-Management-System/Models/Marks.cs
@@ -2,5 +2,20 @@
 {
     public class Marks
     {
+        public int MarksId { get; set; }
+
+        public int StudentId { get; set; }
+        public Student Student { get; set; } = null;
+
+        public int SubjectId { get; set; }
+
+        public Subject Subject { get; set; } = null;
+
+        public int TheoryMarks { get; set; }
+        public int LabMarks { get; set; }
+
+        public int InternalMarks { get; set; }
+
+        public int TotalMarks => TheoryMarks + LabMarks + InternalMarks;
     }
 }
