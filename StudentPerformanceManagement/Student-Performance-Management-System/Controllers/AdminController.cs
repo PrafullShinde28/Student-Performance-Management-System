9395674b151710646e92bdc02fddf76ac76132a7
Prafull Shinde <prafullshinde7007@gmail.com>
2025-12-24 10:28:29 +0530

    Enrollment of student UI and controller updated with view-model

--- StudentPerformanceManagement/Student-Performance-Management-System/Controllers/AdminController.cs
+++ StudentPerformanceManagement/Student-Performance-Management-System/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Student_Performance_Management_System.Models;
+using Student_Performance_Management_System.ViewModel;
 
 namespace Student_Performance_Management_System.Controllers
 {
@@ -22,13 +24,84 @@ namespace Student_Performance_Management_System.Controllers
         {
             return View();
         }
-
-        // ENROLL STUDENT (GET)
-        [HttpGet]
-        public IActionResult EnrollStudent()
+        public IActionResult Students()
+        {
+            var students = _context.Students.ToList();
+            return View(students);
+        }
+        public IActionResult Courses()
         {
             return View();
         }
+
+
+        [HttpGet]
+        public IActionResult EditStudent(int id)
+        {
+            var student = _context.Students.FirstOrDefault(s => s.StudentId == id);
+
+            if (student == null)
+                return NotFound();
+
+            return View(student);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> EditStudent(Student model)
+        {
+            var student = _context.Students.FirstOrDefault(s => s.StudentId == model.StudentId);
+
+            if (student == null)
+                return NotFound();
+
+
+            student.Name = model.Name;
+            student.Email = model.Email;
+            student.MobileNo = model.MobileNo;
+            student.CourseId = model.CourseId;
+            student.CourseGroupId = model.CourseGroupId;
+
+
+            var user = await _userManager.FindByIdAsync(student.AppUserId);
+            if (user != null)
+            {
+                user.Email = model.Email;
+                user.UserName = model.Email;
+                await _userManager.UpdateAsync(user);
+            }
+
+            _context.Students.Update(student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Students");
+        }
+
+
+        // DELETE STUDENT (POST)
+        [HttpPost]
+
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+
+            var student = _context.Students.FirstOrDefault(s => s.StudentId == id);
+            if (student == null)
+                return NotFound();
+
+
+            var user = await _userManager.FindByIdAsync(student.AppUserId);
+
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            if (user != null)
+                await _userManager.DeleteAsync(user);
+
+            return RedirectToAction("Students");
+        }
+
+
+
 
 
         public string GeneratePRN()
@@ -55,19 +128,58 @@ namespace Student_Performance_Management_System.Controllers
             }
         }
 
-
-        // ENROLL STUDENT (POST)
-        [HttpPost]
-        public async Task<IActionResult> EnrollStudent(string name, string email, string mobileno, int courseid, int coursegroupid)
+        [HttpGet]
+        public IActionResult EnrollStudent()
         {
+            var model = new StudentEnrollmentViewModel
+            {
+                Courses = _context.Courses
+                    .Select(c => new SelectListItem
+                    {
+                        Text = c.CourseName.ToString(),
+                        Value = c.CourseId.ToString(),
+                    }).ToList(),
+
+                CourseGroups = _context.CourseGroups
+                    .Select(g => new SelectListItem
+                    {
+                        Text = g.GroupName.ToString(),
+                        Value = g.CourseGroupId.ToString()
+                    }).ToList()
+            };
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> EnrollStudent(StudentEnrollmentViewModel model,
+                                                 StudentEnrollmentViewModel1 model1)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Courses = _context.Courses.Select(c => new SelectListItem
+                {
+                    Text = c.CourseName,
+                    Value = c.CourseId.ToString()
+                }).ToList();
+
+                model.CourseGroups = _context.CourseGroups.Select(g => new SelectListItem
+                {
+                    Text = g.GroupName,
+                    Value = g.CourseGroupId.ToString()
+                }).ToList();
+
+                return View(model);
+            }
+
             string defaultPassword = "Student@123";
 
             var user = new AppUser
             {
-
-                UserName = email,
-                Email = email,
-                FullName = name,
+                UserName = model1.Email,
+                Email = model1.Email,
+                FullName = model1.Name,
                 EmailConfirmed = true
             };
 
@@ -80,15 +192,13 @@ namespace Student_Performance_Management_System.Controllers
                 var student = new Student
                 {
                     PRN = GeneratePRN(),
-                    ProfileImagePath = "default.png",
-                    Name = name,
-                    Email = email,
+                    Name = model1.Name,
+                    Email = model1.Email,
                     AppUserId = user.Id,
-                    MobileNo = mobileno,
-                    CourseId = courseid,
-                    CourseGroupId = coursegroupid,
-
-
+                    MobileNo = model1.MobileNo,
+                    CourseId = model.CourseId,
+                    CourseGroupId = model.CourseGroupId,
+                    ProfileImagePath = model1.ProfileImagePath
                 };
 
                 _context.Students.Add(student);
@@ -97,11 +207,9 @@ namespace Student_Performance_Management_System.Controllers
                 return RedirectToAction("Dashboard", "Account");
             }
 
-            foreach (var error in result.Errors)
-                ModelState.AddModelError("", error.Description);
-
-            return View();
+            return View(model);
         }
+
         // ADD STAFF (GET)
         [HttpGet]
         public IActionResult AddStaff()
--- StudentPerformanceManagement/Student-Performance-Management-System/ViewModel/StudentEnrollmentViewModel.cs
+++ StudentPerformanceManagement/Student-Performance-Management-System/ViewModel/StudentEnrollmentViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace Student_Performance_Management_System.ViewModel
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Student_Performance_Management_System.ViewModel
 {
     public class StudentEnrollmentViewModel
     {
+        [Required(ErrorMessage = "Please select a course")]
+        public int CourseId { get; set; }
+
+        [Required(ErrorMessage = "Please select a course group")]
+        public int CourseGroupId { get; set; }
+
+        public List<SelectListItem> Courses { get; set; } = new();
+        public List<SelectListItem> CourseGroups { get; set; } = new();
     }
+
 }
