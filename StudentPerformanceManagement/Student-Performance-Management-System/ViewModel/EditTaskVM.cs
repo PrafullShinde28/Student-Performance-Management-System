0a5a83cfc0c6629a963f9c6e610fff7aad5d715c
vishwadeep sankpal <vishwadeep108@gmail.com>
2025-12-25 18:05:23 +0530

    Bug Fixing - Edit Task page

---
+++ StudentPerformanceManagement/Student-Performance-Management-System/ViewModel/EditTaskVM.cs
@@ -0,0 +1,17 @@
+﻿namespace Student_Performance_Management_System.ViewModel
+{
+    public class EditTaskVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public int CourseId { get; set; }
+        public int CourseGroupId { get; set; }
+        public int SubjectId { get; set; }
+        public int StaffId { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+    }
+}
