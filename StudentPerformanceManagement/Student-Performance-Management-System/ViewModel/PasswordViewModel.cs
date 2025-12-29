cca65e53bd5f19df45e7b05296d1990175a857ac
swapnil3307 <pols5484@gmail.com>
2025-12-29 15:18:53 +0530

    Added Password view model

---
+++ StudentPerformanceManagement/Student-Performance-Management-System/ViewModel/PasswordViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPerformanceManagment.ViewModel
+{
+    public class PasswordViewModel
+    {
+        public int StudentId { get; set; }
+
+        [Required(ErrorMessage = "Current password is required")]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+        [Required(ErrorMessage = "New password is required")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{6,}$",
+            ErrorMessage = "Password must have at least 1 Uppercase, 1 Lowercase, 1 Number, and 1 Special Character.")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your new password")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
