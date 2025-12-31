d84e0d6e2a226baceabfac265c28fb8bb8b91fa4
vishwadeep sankpal <vishwadeep108@gmail.com>
2025-12-31 16:37:31 +0530

    Implemented Logic for Edit Profile For student and Staff

---
+++ StudentPerformanceManagement/Student-Performance-Management-System/ViewModel/EditStaff.cs
@@ -0,0 +1,11 @@
+﻿namespace Student_Performance_Management_System.ViewModel
+{
+    public class EditStaff
+    {
+        public int StaffId { get; set; }
+        public string StaffName { get; set; }
+        public string Email { get; set; }
+        public string MobileNo { get; set; }
+        public string ProfileImage { get; set; }
+    }
+}
--- StudentPerformanceManagement/Student-Performance-Management-System/ViewModel/StaffDashViewModel.cs
+++ StudentPerformanceManagement/Student-Performance-Management-System/ViewModel/StaffDashViewModel.cs
@@ -4,10 +4,11 @@ namespace Student_Performance_Management_System.ViewModel
 {
     public class StaffDashViewModel
     {
-        public string StaffId { get; set; }
+        public int StaffId { get; set; }
         public string StaffName { get; set; }
         public int TaskCount { get; set; }
-
+        public string Email { get; set; }
+        public string MobileNo { get; set; }
         public string? Profile { get; set; }
         // yeh tumhari Task entity ka type hoga (jo _context.Tasks se aata hai)
         public List<Tasks> Tasks { get; set; } = new List<Tasks>();
